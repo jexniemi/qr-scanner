bb6cb62dc9b9c948bb01d6c91b2335a859fcd3d4
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2020-09-06 15:31:12 +0300

    Prettify

--- screens/QRList/index.js
+++ screens/QRList/index.js
@@ -1,67 +1,82 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { AsyncStorage, View, FlatList, Alert, Text } from 'react-native';
-import ListItem from './ListItem'
-import LoadingLayout from '../_common/LoadingLayout'
-import ClearStorageButton from './ClearStorageButton'
-import ToQRScannerButton from './ToQRScannerButton';
-import mockData from '../../mockData'
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { AsyncStorage, View, FlatList, Alert, Text } from "react-native";
+import ListItem from "./ListItem";
+import LoadingLayout from "../_common/LoadingLayout";
+import ClearStorageButton from "./ClearStorageButton";
+import ToQRScannerButton from "./ToQRScannerButton";
+import mockData from "../../mockData";
 
 export default ({ navigation }) => {
-  const [ scannedCodes, setScannedCodes ] = useState([])
-  const [ isLoading, setIsLoading ] = useState(true)
+  const [scannedCodes, setScannedCodes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  var noScannedCodes = scannedCodes.length === 0
+  var noScannedCodes = scannedCodes.length === 0;
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => <ClearStorageButton onButtonPress={onClearButtonPress} disabled={noScannedCodes} />,
-      headerLeft: () => <ToQRScannerButton navigation={navigation}/>
-    })
-  }, [ navigation, scannedCodes ])
+      headerRight: () => (
+        <ClearStorageButton
+          onButtonPress={onClearButtonPress}
+          disabled={noScannedCodes}
+        />
+      ),
+      headerLeft: () => <ToQRScannerButton navigation={navigation} />,
+    });
+  }, [navigation, scannedCodes]);
 
-	useEffect(() => {
-    _retrieveData()
-  }, [])
+  useEffect(() => {
+    _retrieveData();
+  }, []);
 
   const onClearButtonPress = async () => {
-    AsyncStorage.removeItem('ScannedCodes', setScannedCodes([]), e => Alert.alert(e))
-  }
-
-	const _retrieveData = async () => {
-    // setScannedCodes(JSON.parse(mockData))
-		try {
-			var codes = await AsyncStorage.getItem('ScannedCodes')
-			if (codes != null) {
-				setScannedCodes(JSON.parse(codes).reverse())
-			}
-		} catch (error) {
-      Alert.alert(error)
-    }
-    setIsLoading(false)
+    AsyncStorage.removeItem("ScannedCodes", setScannedCodes([]), (e) =>
+      Alert.alert(e)
+    );
   };
-  
+
+  const _retrieveData = async () => {
+    // setScannedCodes(JSON.parse(mockData))
+    try {
+      var codes = await AsyncStorage.getItem("ScannedCodes");
+      if (codes != null) {
+        setScannedCodes(JSON.parse(codes).reverse());
+      }
+    } catch (error) {
+      Alert.alert(error);
+    }
+    setIsLoading(false);
+  };
+
   if (isLoading) {
-    return <LoadingLayout />
+    return <LoadingLayout />;
   }
 
   return (
-    <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'rgba(240,240,240,1)' }}>
-      { noScannedCodes ? 
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "rgba(240,240,240,1)",
+      }}
+    >
+      {noScannedCodes ? (
         <NoScannedCodesMessage />
-        :
-        <FlatList 
+      ) : (
+        <FlatList
           data={scannedCodes}
-          renderItem={({ item }) => <ListItem content={item.data} date={item.date} />}
-          keyExtractor={item => item.date}
+          renderItem={({ item }) => (
+            <ListItem content={item.data} date={item.date} />
+          )}
+          keyExtractor={(item) => item.date}
           contentContainerStyle={{ marginTop: 8 }}
-			  />
-      }
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
 const NoScannedCodesMessage = () => (
-  <View style={{ flex: 1, justifyContent: 'center'}}>
-    <Text style={{ alignSelf: 'center'}}>Ei skannattuja koodeja.</Text>
+  <View style={{ flex: 1, justifyContent: "center" }}>
+    <Text style={{ alignSelf: "center" }}>Ei skannattuja koodeja.</Text>
   </View>
-)
+);
